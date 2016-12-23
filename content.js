280cbfb827570a49110566e359b1680cfb143aaa
Tiratat P <tiratatp@users.noreply.github.com>
2016-12-23 15:12:27 -0600

    Facebook does not trigger ajax if the page is too short

--- content.js
+++ content.js
@@ -1,10 +1,10 @@
 (function() {
+	"use strict";
 	function remove_ads() {
 		// old version
 		$(".userContentWrapper:has(.uiStreamSponsoredLink)").hide();
 		// new version
-		$(".userContentWrapper:has(._m8d)").hide();
-		$(".userContentWrapper:has(._4dcu)").hide();
+		$(".userContentWrapper:has(._m8c)").hide();
 		// sidebar ads
 		$("#pagelet_ego_pane").remove();
 	}
@@ -12,6 +12,10 @@
 
 	// so that the space will collape nicely when we remove sidebar ad
 	$(".home_right_column").css("min-height", "inherit");
+
+	// make timeline long enough so we could always scroll
+	// so that when we remove ads, we could still scroll to trigger ajax load
+	$("#stream_pagelet").css("height", "10000px");
 
 	// remove ads on the first load
 	remove_ads();
