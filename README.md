112582cc59d3cd9b1fbb37961e2ebd0ed9a8ac92
Tiratat P <tiratatp@users.noreply.github.com>
2017-02-22 16:09:40 -0600

    Optimize more
    - remove js code for mobile since we can use css
    - make mutationobserver more specific to improve performance

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 An open-source Ad Blocker for Facebook™ Chrome extension
 - Blocks both suggested posts and sidebar ads
 - Does not slow down your computer
-- Does not track/read your Facebook activity or other websites
-- Only has 57 lines of code and no external dependency! https://github.com/tiratatp/facebook_adblock/blob/master/content.js
+- Does not track/read your Facebook activity, or other websites
+- Only has 49 lines of code, and no external dependency! https://github.com/tiratatp/facebook_adblock/blob/master/content.js
 
 This extension is 100% free and open source.
 
@@ -15,4 +15,5 @@ https://addons.mozilla.org/en-US/firefox/addon/fb_ad_block/
 
 Enjoy!
 
-_We are not affiliated, associated, authorized, endorsed by, or in any way officially connected with Facebook. Facebook is a registered trademark of the Facebook, Inc._
+_We are not affiliated, associated, authorized, endorsed by, or in any way officially connected with Facebook._
+_Facebook is a registered trademark of the Facebook, Inc._
--- content.js
+++ content.js
@@ -1,45 +1,31 @@
 (function() {
 	"use strict";
-	function hide(e){
-		e && (e.style.display = "none")
-	}
-
 	function hide_if_sponsored(e) {
-		0 != e.getElementsByClassName("_m8c").length && hide(e)
+		// if e contains ._m8c, then hide
+		0 != e.getElementsByClassName("_m8c").length && (e.style.display = "none")
 	}
 
 	function remove_sponsored_posts(c) {
-		if (typeof c.getElementsByClassName == "function") {
-			c.querySelectorAll(".userContentWrapper").forEach(hide_if_sponsored);
-
-			// new version has ".fbUserContent" instead of .userContentWrapper
-			c.querySelectorAll(".fbUserContent").forEach(hide_if_sponsored);
-		}
+		// new version has ".fbUserContent" instead of .userContentWrapper
+		c.querySelectorAll(".fbUserContent, .userContentWrapper").forEach(hide_if_sponsored);
 	}
 
-	function onBodyChange() {
-		var new_root_tag;
-
-		if (!isMobile) {
-			new_root_tag = document.getElementById("stream_pagelet");
-		} else {
-			new_root_tag = document.getElementById("MRoot");
-		}
+	function onPageChange() {
+		var new_root_tag = document.getElementById("stream_pagelet");
 
 		// if the user change page to/from homepage
 		if (new_root_tag !== root_tag) {
-			// Adding observers
+			// removing old observers
 			stream_observer.disconnect();
+			// add to new tag
 			new_root_tag && stream_observer.observe(new_root_tag, {childList: true, subtree: true});
 			root_tag = new_root_tag;
 		}
 	}
 
-	// isMobile is from Facebook SDK for JavaScript
-	var h = navigator.userAgent,
-		isMobile = /\b(iPhone|iP[ao]d)/.test(h) || /\b(iP[ao]d)/.test(h) || /Android/i.test(h) || /Mobile/i.test(h),
-		root_tag,
-		body_observer = new MutationObserver(onBodyChange),
+	var fb_content = document.getElementsByClassName("fb_content")[0],		
+		fb_observer = new MutationObserver(onPageChange),
+		root_tag = null,
 		stream_observer = new MutationObserver(function(mutations) {
 			mutations.forEach(function(mutation) {
 				if (mutation.target.dataset.cursor) {
@@ -48,10 +34,15 @@
 			});
 		});
 
-	// remove on first load
-	onBodyChange();
-	remove_sponsored_posts(document.body);
+	// if we can't find ".fb_content", then it must be a mobile website.
+	// in that case, we don't need javascript to block ads
+	if (fb_content) {
+		// remove on first load
+		onPageChange();
+		remove_sponsored_posts(fb_content);
 
-	// watch for page change
-	body_observer.observe(document.body, {childList: true});
+		// Facebook uses ajax to load new content so
+		// we need this to watch for page change
+		fb_observer.observe(fb_content, {childList: true});
+	}
 }());
