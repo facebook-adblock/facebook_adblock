0083a32240592b959c82f9249702305ddbc61fa5
Tiratat P <tiratatp@users.noreply.github.com>
2017-03-15 19:03:16 -0500

    Try to block ads in group
    - Refactor variable names and function names
    - Use more universal filters

--- content.js
+++ content.js
@@ -1,59 +1,65 @@
 (function() {
     "use strict";
-    function hide_if_sponsored(e) {
+    function hideIfSponsored(e) {
         // if e contains ._m8c or .uiStreamSponsoredLink, then hide
-        ["_m8c", "uiStreamSponsoredLink"].forEach(function(className) {
-            if (e.getElementsByClassName(className).length !== 0) {
+        // a[data-hovercard][href*="hc_ref=ADS"] from https://github.com/uBlockOrigin/uAssets/issues/233
+        var queries = ['._m8c', '.uiStreamSponsoredLink', 'a[data-hovercard][href*="hc_ref=ADS"]'];
+        return queries.some(function(query) {
+            if (e.querySelector(query) !== null) {
                 e.style.display = "none";
-                console.info("AD Blocked", e);
+                console.info("AD Blocked", [e]);
+                return true;
             }
-        });
-    }
-
-    function remove_sponsored_posts(c) {
-        // new version has ".fbUserContent" instead of .userContentWrapper
-        ["fbUserContent", "userContentWrapper"].forEach(function(className) {
-            Array.prototype.forEach.call(c.getElementsByClassName(className), hide_if_sponsored);
+            return false;
         });
     }
 
     function onPageChange() {
-        var contentArea = document.getElementById("contentArea");
-        // if the user change page to homepage
-        if (contentArea !== null) {
-            remove_sponsored_posts(contentArea);
-            stream_observer.observe(contentArea, {childList: true, subtree: true});
-        } else {
-            // removing old observers
-            stream_observer.disconnect();
+        var feed;
+        if ((feed = document.getElementById("stream_pagelet")) !== null) {
+            // if the user change page to homepage
+            feed.querySelectorAll('div[id^="hyperfeed_story_id_"]').forEach(hideIfSponsored);
+            feedObserver = new MutationObserver(function(mutations) {
+                mutations.forEach(function (mutation) {
+                    if (mutation.target.id.startsWith("hyperfeed_story_id_")) {
+                        hideIfSponsored(mutation.target);
+                    }
+                });
+            });
+            feedObserver.observe(feed, {childList: true, subtree: true});
+        } else if ((feed = document.getElementById("pagelet_group_mall")) !== null) {
+            // if the user change page to https://www.facebook.com/groups/*
+            feed.querySelectorAll('div[id^="mall_post_"]').forEach(hideIfSponsored);
+            feedObserver = new MutationObserver(function(mutations) {
+                mutations.forEach(function (mutation) {
+                    mutation.target.querySelectorAll('div[id^="mall_post_"]').forEach(hideIfSponsored);
+                });
+            });
+            feedObserver.observe(feed, {childList: true, subtree: true});
         }
     }
 
-    var fb_content = document.getElementsByClassName("fb_content")[0],
-        stream_observer = new MutationObserver(function(mutations) {
-            mutations.forEach(function(mutation) {
-                if (mutation.target.dataset.hasOwnProperty("cursor")) {
-                    remove_sponsored_posts(mutation.target);
-                }
-            });
-        }),
-        fb_observer = new MutationObserver(onPageChange);
+    var fbContent = document.getElementsByClassName("fb_content")[0],
+        feedObserver = null,
+        fbObserver = new MutationObserver(onPageChange);
 
     // if we can't find ".fb_content", then it must be a mobile website.
     // in that case, we don't need javascript to block ads
-    if (fb_content !== undefined) {
+    if (fbContent !== undefined) {
         // remove on first load
         onPageChange();
 
         // Facebook uses ajax to load new content so
         // we need this to watch for page change
-        fb_observer.observe(fb_content, {childList: true});
+        fbObserver.observe(fbContent, {childList: true});
     }
 
     // cleanup
     window.addEventListener("beforeunload", function() {
-        fb_observer.disconnect();
-        stream_observer.disconnect();
-        fb_content = null;
+        fbObserver.disconnect();
+        if (feedObserver !== null) {
+            feedObserver.disconnect();
+        }        
+        fbContent = null;
     });
 }());
