36db6779d78ff72e798d4654627459fe4c890b77
Tiratat P <tiratatp@users.noreply.github.com>
2018-11-09 09:47:15 -0800

    Reformat code

--- content.js
+++ content.js
@@ -5,63 +5,63 @@
     // Reference: http://es5.github.io/#x15.4.4.18
     // From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach#Polyfill
     if (!Array.prototype.forEach) {
-      Array.prototype.forEach = function(callback/*, thisArg*/) {
-        var T, k;
+        Array.prototype.forEach = function(callback /*, thisArg*/ ) {
+            var T, k;
 
-        if (this == null) {
-          throw new TypeError('this is null or not defined');
-        }
+            if (this == null) {
+                throw new TypeError('this is null or not defined');
+            }
 
-        // 1. Let O be the result of calling toObject() passing the
-        // |this| value as the argument.
-        var O = Object(this);
+            // 1. Let O be the result of calling toObject() passing the
+            // |this| value as the argument.
+            var O = Object(this);
 
-        // 2. Let lenValue be the result of calling the Get() internal
-        // method of O with the argument "length".
-        // 3. Let len be toUint32(lenValue).
-        var len = O.length >>> 0;
+            // 2. Let lenValue be the result of calling the Get() internal
+            // method of O with the argument "length".
+            // 3. Let len be toUint32(lenValue).
+            var len = O.length >>> 0;
 
-        // 4. If isCallable(callback) is false, throw a TypeError exception.
-        // See: http://es5.github.com/#x9.11
-        if (typeof callback !== 'function') {
-          throw new TypeError(callback + ' is not a function');
-        }
+            // 4. If isCallable(callback) is false, throw a TypeError exception.
+            // See: http://es5.github.com/#x9.11
+            if (typeof callback !== 'function') {
+                throw new TypeError(callback + ' is not a function');
+            }
 
-        // 5. If thisArg was supplied, let T be thissArg; else let
-        // T be undefined.
-        if (arguments.length > 1) {
-          T = arguments[1];
-        }
+            // 5. If thisArg was supplied, let T be thisArg; else let
+            // T be undefined.
+            if (arguments.length > 1) {
+                T = arguments[1];
+            }
 
-        // 6. Let k be 0
-        k = 0;
+            // 6. Let k be 0.
+            k = 0;
 
-        // 7. Repeat, while k < len
-        while (k < len) {
+            // 7. Repeat while k < len.
+            while (k < len) {
 
-          var kValue;
+                var kValue;
 
-          // a. Let Pk be ToString(k).
-          //    This is implicit for LHS operands of the in operator
-          // b. Let kPresent be the result of calling the HasProperty
-          //    internal method of O with argument Pk.
-          //    This step can be combined with c
-          // c. If kPresent is true, then
-          if (k in O) {
+                // a. Let Pk be ToString(k).
+                //    This is implicit for LHS operands of the in operator.
+                // b. Let kPresent be the result of calling the HasProperty
+                //    internal method of O with argument Pk.
+                //    This step can be combined with c.
+                // c. If kPresent is true, then
+                if (k in O) {
 
-            // i. Let kValue be the result of calling the Get internal
-            // method of O with argument Pk.
-            kValue = O[k];
+                    // i. Let kValue be the result of calling the Get internal
+                    // method of O with argument Pk.
+                    kValue = O[k];
 
-            // ii. Call the Call internal method of callback with T as
-            // the this value and argument list containing kValue, k, and O.
-            callback.call(T, kValue, k, O);
-          }
-          // d. Increase k by 1.
-          k++;
-        }
-        // 8. return undefined
-      };
+                    // ii. Call the Call internal method of callback with T as
+                    // the this value and argument list containing kValue, k, and O.
+                    callback.call(T, kValue, k, O);
+                }
+                // d. Increase k by 1.
+                k++;
+            }
+            // 8. return undefined.
+        };
     }
 
     function hideIfSponsored(e) {
@@ -106,22 +106,28 @@
             // if the user change page to homepage
             feed.querySelectorAll('div[id^="hyperfeed_story_id_"]').forEach(hideIfSponsored);
             feedObserver = new MutationObserver(function(mutations) {
-                mutations.forEach(function (mutation) {
+                mutations.forEach(function(mutation) {
                     if (mutation.target.id.startsWith("hyperfeed_story_id_")) {
                         hideIfSponsored(mutation.target);
                     }
                 });
             });
-            feedObserver.observe(feed, {childList: true, subtree: true});
+            feedObserver.observe(feed, {
+                childList: true,
+                subtree: true
+            });
         } else if ((feed = document.getElementById("pagelet_group_")) !== null) {
             // if the user change page to https://www.facebook.com/groups/*
             feed.querySelectorAll('div[id^="mall_post_"]').forEach(hideIfSponsored);
             feedObserver = new MutationObserver(function(mutations) {
-                mutations.forEach(function (mutation) {
+                mutations.forEach(function(mutation) {
                     mutation.target.querySelectorAll('div[id^="mall_post_"]').forEach(hideIfSponsored);
                 });
             });
-            feedObserver.observe(feed, {childList: true, subtree: true});
+            feedObserver.observe(feed, {
+                childList: true,
+                subtree: true
+            });
         }
         console.info("Monitoring", [feed]);
     }
@@ -138,7 +144,9 @@
 
         // Facebook uses ajax to load new content so
         // we need this to watch for page change
-        fbObserver.observe(fbContent, {childList: true});
+        fbObserver.observe(fbContent, {
+            childList: true
+        });
     }
 
     // cleanup
@@ -146,7 +154,7 @@
         fbObserver.disconnect();
         if (feedObserver !== null) {
             feedObserver.disconnect();
-        }        
+        }
         fbContent = null;
     });
 }());
