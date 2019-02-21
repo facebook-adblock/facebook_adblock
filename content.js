10f62270f8953951e6248b1a33aeef45a72b22b0
Tiratat P <tiratatp@users.noreply.github.com>
2019-02-21 00:33:46 -0800

    Support other languages + Eslint

--- content.js
+++ content.js
@@ -1,30 +1,29 @@
-(function() {
-  "use strict";
-
+const adblock = function adblock() {
   // Production steps of ECMA-262, Edition 5, 15.4.4.18
   // Reference: http://es5.github.io/#x15.4.4.18
   // From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach#Polyfill
   if (!Array.prototype.forEach) {
-    Array.prototype.forEach = function(callback /*, thisArg*/) {
-      var T, k;
+    Array.prototype.forEach = function forEach(callback /* , thisArg */) {
+      let T;
+      let k;
 
       if (this == null) {
-        throw new TypeError("this is null or not defined");
+        throw new TypeError('this is null or not defined');
       }
 
       // 1. Let O be the result of calling toObject() passing the
       // |this| value as the argument.
-      var O = Object(this);
+      const O = Object(this);
 
       // 2. Let lenValue be the result of calling the Get() internal
       // method of O with the argument "length".
       // 3. Let len be toUint32(lenValue).
-      var len = O.length >>> 0;
+      const len = O.length >>> 0;
 
       // 4. If isCallable(callback) is false, throw a TypeError exception.
       // See: http://es5.github.com/#x9.11
-      if (typeof callback !== "function") {
-        throw new TypeError(callback + " is not a function");
+      if (typeof callback !== 'function') {
+        throw new TypeError(`${callback} is not a function`);
       }
 
       // 5. If thisArg was supplied, let T be thisArg; else let
@@ -38,7 +37,7 @@
 
       // 7. Repeat while k < len.
       while (k < len) {
-        var kValue;
+        let kValue;
 
         // a. Let Pk be ToString(k).
         //    This is implicit for LHS operands of the in operator.
@@ -56,37 +55,76 @@
           callback.call(T, kValue, k, O);
         }
         // d. Increase k by 1.
-        k++;
+        k += 1;
       }
       // 8. return undefined.
     };
   }
 
+  // if e contains anything in whitelist, then ignore.
+  const whitelist = [];
+
+  // if e contains anything in blacklist, then hide.
+  // a[data-hovercard][href*="hc_ref=ADS"] from https://github.com/uBlockOrigin/uAssets/issues/233
+  // a[role="button"][target="_blank"] is used for good post now too.
+  const blacklist = [
+    '._m8c',
+    '.uiStreamSponsoredLink',
+    'a[data-hovercard][href*="hc_ref=ADS"]',
+    'a[role="button"][rel~="noopener"][data-lynx-mode="async"]',
+  ];
+
+  const sponsoredTexts = [
+    'Sponsored',
+    'प्रायोजित', // Hindi
+    'Patrocinado', // Portuguese (Brazil)
+    'Bersponsor', // Indonesian
+    'Publicidad', // Spanish
+    'May Sponsor', // Filipino
+    'Được tài trợ', // Vietnamese
+    'ได้รับการสนับสนุน', // Thai
+    'Sponsorlu', // Turkish
+    '赞助内容', // Chinese Simplified
+    '贊助', // Chinese (Taiwan)
+    'Sponsorisé', // French
+    'Gesponsert', // German
+  ];
+
+  function isHidden(e) {
+    const style = window.getComputedStyle(e);
+    if (
+      style.display === 'none'
+      || style.opacity === '0'
+      || style.fontSize === '0px'
+      || style.visibility === 'hidden'
+    ) {
+      return true;
+    }
+    return false;
+  }
+
   function getVisibleText(e) {
-    if (window.getComputedStyle(e).display === "none") {
+    if (isHidden(e)) {
       // stop if this is hidden
       return [];
     }
-    var children = e.querySelectorAll(":scope > s");
+    const children = e.querySelectorAll(':scope > *');
     if (children.length !== 0) {
       // more level => recursive
       return Array.prototype.slice
         .call(children)
         .map(getVisibleText)
         .flat();
-    } else {
-      // we have found the real text
-      return e.innerText;
     }
+    // we have found the real text
+    return e.innerText;
   }
 
   function hideIfSponsored(e) {
-    // if e contains anything in whitelist, then ignore.
-    var whitelist = [];
     if (
-      whitelist.some(function(query) {
+      whitelist.some((query) => {
         if (e.querySelector(query) !== null) {
-          console.info("Ignored (" + query + ")", [e]);
+          console.info(`Ignored (${query})`, [e]);
           return true;
         }
         return false;
@@ -95,20 +133,11 @@
       return false; // ignored this element
     }
 
-    // if e contains anything in blacklist, then hide.
-    // a[data-hovercard][href*="hc_ref=ADS"] from https://github.com/uBlockOrigin/uAssets/issues/233
-    // a[role="button"][target="_blank"] is used for good post now too.
-    var blacklist = [
-      "._m8c",
-      ".uiStreamSponsoredLink",
-      'a[data-hovercard][href*="hc_ref=ADS"]',
-      'a[role="button"][rel~="noopener"][data-lynx-mode="async"]'
-    ];
     if (
-      blacklist.some(function(query) {
+      blacklist.some((query) => {
         if (e.querySelector(query) !== null) {
-          e.style.display = "none";
-          console.info("AD Blocked (" + query + ")", [e]);
+          e.style.display = 'none';
+          console.info(`AD Blocked (${query})`, [e]);
           return true;
         }
         return false;
@@ -117,51 +146,53 @@
       return true; // has ad
     }
 
-    var possibleSponsoredTags = e.querySelectorAll('div[id^="feed_sub_title"] span > s');
-    possibleSponsoredTags.forEach(function(t) {
-      if (getVisibleText(t).join("") === "Sponsored") {
-        e.style.display = "none";
-        console.info("AD Blocked (getVisibleText())", [e]);
+    const possibleSponsoredTags = e.querySelectorAll('div[id^="feed_sub_title"] > :first-child');
+    possibleSponsoredTags.forEach((t) => {
+      if (sponsoredTexts.indexOf(getVisibleText(t).join('')) !== -1) {
+        e.style.display = 'none';
+        console.info('AD Blocked (getVisibleText())', [e]);
         return true;
       }
+      return false;
     });
+    return false;
   }
 
+  let feedObserver = null;
   function onPageChange() {
-    var feed;
-    if ((feed = document.getElementById("stream_pagelet")) !== null) {
+    let feed;
+    if ((feed = document.getElementById('stream_pagelet')) !== null) {
       // if the user change page to homepage
       feed.querySelectorAll('div[id^="hyperfeed_story_id_"]').forEach(hideIfSponsored);
-      feedObserver = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
-          if (mutation.target.id.startsWith("hyperfeed_story_id_")) {
+      feedObserver = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+          if (mutation.target.id.startsWith('hyperfeed_story_id_')) {
             hideIfSponsored(mutation.target);
           }
         });
       });
       feedObserver.observe(feed, {
         childList: true,
-        subtree: true
+        subtree: true,
       });
-    } else if ((feed = document.getElementById("pagelet_group_")) !== null) {
+    } else if ((feed = document.getElementById('pagelet_group_')) !== null) {
       // if the user change page to https://www.facebook.com/groups/*
       feed.querySelectorAll('div[id^="mall_post_"]').forEach(hideIfSponsored);
-      feedObserver = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
+      feedObserver = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
           mutation.target.querySelectorAll('div[id^="mall_post_"]').forEach(hideIfSponsored);
         });
       });
       feedObserver.observe(feed, {
         childList: true,
-        subtree: true
+        subtree: true,
       });
     }
-    console.info("Monitoring", [feed]);
+    console.info('Monitoring', [feed]);
   }
 
-  var fbContent = document.getElementsByClassName("fb_content")[0],
-    feedObserver = null,
-    fbObserver = new MutationObserver(onPageChange);
+  let fbContent = document.getElementsByClassName('fb_content')[0];
+  const fbObserver = new MutationObserver(onPageChange);
 
   // if we can't find ".fb_content", then it must be a mobile website.
   // in that case, we don't need javascript to block ads
@@ -172,16 +203,17 @@
     // Facebook uses ajax to load new content so
     // we need this to watch for page change
     fbObserver.observe(fbContent, {
-      childList: true
+      childList: true,
     });
   }
 
   // cleanup
-  window.addEventListener("beforeunload", function() {
+  window.addEventListener('beforeunload', () => {
     fbObserver.disconnect();
     if (feedObserver !== null) {
       feedObserver.disconnect();
     }
     fbContent = null;
   });
-})();
+};
+adblock();
