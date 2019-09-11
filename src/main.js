62ec0fa2d956f02480c400955f3c34f69a788a25
Tiratat P <tiratatp@users.noreply.github.com>
2019-09-10 21:06:23 -0700

    Support Hebrew

--- src/main.js
+++ src/main.js
@@ -21,6 +21,7 @@ const sponsoredTexts = [
   'May Sponsor', // Filipino
   'Sponsorisé', // French
   'Gesponsert', // German
+  'ממומן', // Hebrew
   'प्रायोजित', // Hindi
   'Bersponsor', // Indonesian
   'Sponsorizzato', // Italian
