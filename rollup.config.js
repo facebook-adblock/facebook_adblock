2da5f3cb80f398151a7901a6c73018257169b80a
Tiratat Patana-anake <tiratatp@users.noreply.github.com>
2020-12-25 16:57:45 -0800

    Upgrade Rollup's plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,23 @@
-const babel = require("rollup-plugin-babel");
-const cleanup = require("rollup-plugin-cleanup");
-const commonjs = require("rollup-plugin-commonjs");
-const resolve = require("rollup-plugin-node-resolve");
+import babel from "@rollup/plugin-babel";
+import cleanup from "rollup-plugin-cleanup";
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 
-module.exports = {
+const config = {
   input: "src/main.js",
   output: {
     file: "dist/src/content.js",
     format: "iife", // immediately-invoked function expression — suitable for <script> tags
   },
   plugins: [
-    resolve(),
+    nodeResolve(),
     babel({
+      babelHelpers: "bundled",
       exclude: "node_modules/**",
     }),
     commonjs(),
     cleanup(),
   ],
 };
+
+export default config;
