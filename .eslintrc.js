79d68afe241c70925732fd267987aba6fb6c9459
Tiratat Patana-anake <tiratatp@users.noreply.github.com>
2020-12-25 16:08:03 -0800

    Fix formatting for `.eslintrc.js`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
   extends: ["airbnb-base", "plugin:prettier/recommended"],
   parserOptions: {
     ecmaVersion: 6,
-    sourceType: "module"
-  }
+    sourceType: "module",
+  },
 };
