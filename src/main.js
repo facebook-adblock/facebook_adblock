302dc7af02debd3c55a81172a7ebc9bb0be38437
Hoang Ho Hai Dang <haidang@bacoor.co>
2021-02-05 17:30:34 +0700

    WIP

--- src/main.js
+++ src/main.js
@@ -3,13 +3,22 @@ import {
   isClassicFacebook,
 } from "./classic";
 import { setupPageObserver as setupFB5PageObserver, isFB5 } from "./fb5";
+import {
+  setupPageObserver as setupFB5WatchObserver,
+  isFBWatch,
+} from "./fb_watch";
 
 if (isClassicFacebook()) {
   // Old Facebook design
   setupClassicPageObserver();
 } else if (isFB5()) {
   // if it's FB5 design
-  setupFB5PageObserver();
+  if (isFBWatch()) {
+    // if it's facebook.com/watch
+    setupFB5WatchObserver();
+  } else {
+    setupFB5PageObserver();
+  }
 } else {
   console.warn(
     "Page element not found! If this is not a mobile Facebook, please file a bug report: https://github.com/tiratatp/facebook_adblock/issues/new"
@@ -17,3 +26,6 @@ if (isClassicFacebook()) {
 }
 // if we cannot detect a page element, then it must be a mobile website.
 // in that case, we don't need javascript to block ads.
+
+//dom = document.querySelectorAll('div[aria-haspopup="menu"]')[0]
+//dom.parentElement.parentElement.parentElement.parentElement.parentElement.style.display = "none"
