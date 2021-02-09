775aedb1441f705ad8968ab068d98155eb7c6803
Hoàng Hồ Hải Đăng <hdcompany123@gmail.com>
2021-02-09 08:22:03 +0700

    Hide ads in fb/watch (#82)
    * remove ads on fb/watch
    * fix observer when navigating between newfeed & watch
    * refactor code
    
    Co-authored-by: Hoang Ho Hai Dang <haidang@bacoor.co>
    Co-authored-by: Felix Hoang <haidang@2589.com>

--- src/fb5.js
+++ src/fb5.js
@@ -9,7 +9,25 @@ function hideIfSponsored(e) {
   return __hideIfSponsored(possibleSponsoredTextQueries, e);
 }
 
+/**
+ * check the element is a sponsored video
+ */
+function hideVideoIfSponsored(e) {
+  // check sponsored video condition
+  const childNode = e.querySelector(
+    'div[aria-haspopup="menu"]:not([data-adblocked])'
+  );
+  if (childNode !== null) {
+    childNode.dataset.adblocked = true;
+    // flag a sponsored video
+    e.style.display = "none";
+    e.dataset.blocked = "sponsored";
+    console.info(`AD Blocked (div[aria-haspopup="menu"])`, [childNode, e]);
+  }
+}
+
 let feedObserver = null;
+let watchObserver = null;
 
 // wait for and observe FB5 feed element
 function setFeedObserver() {
@@ -67,7 +85,70 @@ function setFeedObserver() {
   }
 }
 
+// wait for and observe FB5 feed element
+function setWatchObserver() {
+  // We are expecting to find a new feed div
+  const feed = document.querySelector(
+    'div[data-pagelet="MainFeed"]>div>div>div:not([data-adblock-monitored]):first-child'
+  );
+
+  if (feed !== null) {
+    // flag as monitored
+    feed.dataset.adblockMonitored = true;
+    const feedContainer = feed.parentNode;
+    watchObserver = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        // check if feed was reloaded without changing page
+        if (
+          mutation.target === feedContainer &&
+          mutation.addedNodes.length > 0
+        ) {
+          watchObserver.disconnect();
+          // check again for the new feed. Since the DOM has just changed, we
+          // want to wait a bit and start looking for the new div after it was
+          // rendered. We put our method at the end of the current queue stack
+          setTimeout(setWatchObserver, 0);
+        }
+        // new feed posts added
+        if (mutation.target === feed && mutation.addedNodes.length > 0) {
+          mutation.addedNodes.forEach((node) => {
+            hideVideoIfSponsored(node);
+          });
+        }
+      });
+    });
+    // check for new feed posts
+    watchObserver.observe(feed, {
+      childList: true,
+    });
+    // check if the feed is replaced
+    watchObserver.observe(feedContainer, {
+      childList: true,
+    });
+    console.info("Monitoring watch updates", [feed]);
+  } else {
+    // no feed div was available yet in DOM. will check again
+    setTimeout(setWatchObserver, 1000);
+  }
+}
+
 function onPageChange() {
+  if (isFBWatch()) {
+    if (feedObserver !== null) {
+      feedObserver.disconnect();
+      feedObserver = null;
+    }
+    onPageChangeInWatch();
+  } else {
+    if (watchObserver !== null) {
+      watchObserver.disconnect();
+      watchObserver = null;
+    }
+    onPageChangeInNewFeed();
+  }
+}
+
+function onPageChangeInNewFeed() {
   // there's a feed div that we don't monitor yet
   if (
     document.querySelector("div[role=feed]:not([data-adblock-monitored])") !==
@@ -89,6 +170,34 @@ function onPageChange() {
   ) {
     feedObserver.disconnect();
     feedObserver = null;
+  }
+}
+
+function onPageChangeInWatch() {
+  // there's a feed div that we don't monitor yet
+  if (
+    document.querySelector(
+      'div[data-pagelet="MainFeed"]>div>div>div:not([data-adblock-monitored]):first-child'
+    ) !== null
+  ) {
+    setWatchObserver();
+    return;
+  }
+  // there's a feed loading placeholder
+  if (document.querySelector('div[role="progressbar"]') !== null) {
+    setWatchObserver();
+    return;
+  }
+  // No new feed was detected
+  // Cleanup observer when there's no feed monitored left in DOM
+  if (
+    watchObserver !== null &&
+    document.querySelector(
+      'div[data-pagelet="MainFeed"]>div>div>div:first-child[data-adblock-monitored]'
+    ) === null
+  ) {
+    watchObserver.disconnect();
+    watchObserver = null;
   }
 }
 
@@ -122,9 +231,15 @@ function setupPageObserver() {
 // cleanup
 window.addEventListener("beforeunload", () => {
   pageObserver.disconnect();
+
   if (feedObserver !== null) {
     feedObserver.disconnect();
     feedObserver = null;
+  }
+
+  if (watchObserver !== null) {
+    watchObserver.disconnect();
+    watchObserver = null;
   }
 });
 
@@ -134,6 +249,14 @@ window.addEventListener("beforeunload", () => {
  */
 function isFB5() {
   return document.getElementById("mount_0_0") !== null;
+}
+
+/**
+ * Detect if it is on fb/watch
+ * @returns {boolean} true if this is on fb/watch
+ */
+function isFBWatch() {
+  return document.location.pathname === "/watch";
 }
 
 export { setupPageObserver, isFB5 };
