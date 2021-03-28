597db77a02c621ed35dad2bf53164e775b9af387
Nadav S <nadavsn@users.noreply.github.com>
2021-03-28 22:49:53 +0300

    Make the "paid for by ..." selector more specific. the `:not` part omits buttons under the "suggested friends" feed item.

--- src/fb5.js
+++ src/fb5.js
@@ -3,7 +3,7 @@ import __hideIfSponsored from "./hide_if_sponsored";
 const possibleSponsoredTextQueries = [
   'a[role="link"] > span[aria-labelledby]',
   'div[role="button"] > span[aria-labelledby]',
-  'div[role="button"]',
+  'span[dir="auto"] > span > div[role="button"]:not([aria-labelledby])',
 ];
 
 function hideIfSponsored(e) {
